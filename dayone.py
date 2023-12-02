160cc3c81d54ad148a8033e67fd8914c43a54a6e
N-M <nmonforte@gmail.com>
2023-12-02 16:57:31 -0500

    Day One Solve

--- dayone.py
+++ dayone.py
@@ -1,16 +1,17 @@
-f = open("./dayoneinput.txt", "r")
-sumCalibration = 0
-for line in f:
-    if len(line) > 0:
-        first = "UNKNOWN"
-        last = "UNKNOWN"
-        for char in line:
-            if char.strip().isdigit():
-                if first == "UNKNOWN":
-                    first = char.strip()
-                last = char.strip()
-        print(f"{first}{last}")
-        sumCalibration += int(f"{first}{last}")
+def SolveDayOne_PartOne():
+    f = open("./dayoneinput.txt", "r")
+    sumCalibration = 0
+    for line in f:
+        if len(line) > 0:
+            first = "UNKNOWN"
+            last = "UNKNOWN"
+            for char in line:
+                if char.strip().isdigit():
+                    if first == "UNKNOWN":
+                        first = char.strip()
+                    last = char.strip()
+            print(f"{first}{last}")
+            sumCalibration += int(f"{first}{last}")
 
-print(sumCalibration)
-f.close()
+    print(sumCalibration)
+    f.close()
