4d5c12e10621aab61291e468f3bdd9628eef721f
N-M <nmonforte@gmail.com>
2023-12-02 12:14:51 -0500

    Part 1 complete

---
+++ dayone.py
@@ -0,0 +1,16 @@
+f = open("./dayoneinput.txt", "r")
+sumCalibration = 0
+for line in f:
+    if len(line) > 0:
+        first = "UNKNOWN"
+        last = "UNKNOWN"
+        for char in line:
+            if char.strip().isdigit():
+                if first == "UNKNOWN":
+                    first = char.strip()
+                last = char.strip()
+        print(f"{first}{last}")
+        sumCalibration += int(f"{first}{last}")
+
+print(sumCalibration)
+f.close()
